ea9c839c88f7f4ab9072ee6015638c5d3707d65e
ashika33 <45399045+ashika33@users.noreply.github.com>
2018-12-03 12:03:13 -0500

    commit 2

---
+++ GitTest.java
@@ -0,0 +1,22 @@
+public class GitTest{
+
+	private String variable;
+
+
+	public GitTest(){
+		this.variable = variable;
+
+
+	}
+
+	public String toString(){
+		return "This is my Github test";
+
+	}
+
+
+
+
+
+
+}
